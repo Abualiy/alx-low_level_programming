532725372ffd53e6a4ff92b27e23c46021d476d2
Abualiy <akremmuktar332@gmail.com>
2022-09-15 00:49:12 -0700

    is digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,13 @@
 #define MAIN_H
 
 int _isupper(int c);
-
+int _isdigit(int c);
+void print_numbers(void);
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+int mul(int a, int b);
 #endif /* MAIN_H */
